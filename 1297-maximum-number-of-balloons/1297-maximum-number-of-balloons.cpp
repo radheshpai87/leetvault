ea0c231c35cff6c524697dee99917cfa87b55e75
Radhesh Pai <radhesh.ratnakar.s87@kalvium.community>
2026-06-22 22:50:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (64.87%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         int count = 0;
-        int counta = 0, countb = 0;
-        int countl = 0, counto = 0, countn = 0;
+        int counta = 0; 
+        int countb = 0;
+        int countl = 0; 
+        int counto = 0; 
+        int countn = 0;
 
         for(char x : text) {
             switch(x) {
@@ -14,12 +17,8 @@ public:
                 case 'n': countn++; break;
             }
 
-            if(countb >= 1 && counta >= 1 &&
-               countl >= 2 && counto >= 2 &&
-               countn >= 1)
-            {
+            if(countb >= 1 && counta >= 1 && countl >= 2 && counto >= 2 && countn >= 1){
                 count++;
-
                 countb -= 1;
                 counta -= 1;
                 countl -= 2;
@@ -27,7 +26,6 @@ public:
                 countn -= 1;
             }
         }
-
         return count;
     }
 };
